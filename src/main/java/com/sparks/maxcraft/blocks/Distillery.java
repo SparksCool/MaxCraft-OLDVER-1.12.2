4b4e6df5b141464405bd6f4abe86caffdf5f0f21
SparksCool <70763188+SparksCool@users.noreply.github.com>
2021-04-29 19:37:47 -0400

    Working on gui for distillery

--- src/main/java/com/sparks/maxcraft/blocks/Distillery.java
+++ src/main/java/com/sparks/maxcraft/blocks/Distillery.java
@@ -1,5 +1,6 @@
 package sparks.maxcraft.blocks;
 import sparks.maxcraft.MaxCraft;
+import sparks.maxcraft.tilentities.*;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -20,5 +21,25 @@ public class Distillery extends Block {
     public void initModel() {
         ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
     }
+    @Override
+    public TileEntity createNewTileEntity(World worldIn, int meta) {
+        return new DistilleryTileEntity();
+    }
+
+    @Override
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, ItemStack heldItem, EnumFacing side,
+                float hitX, float hitY, float hitZ) {
+        // Only execute on the server
+        if (world.isRemote) {
+            return true;
+        }
+        TileEntity te = world.getTileEntity(pos);
+        if (!(te instanceof TestContainerTileEntity)) {
+            return false;
+        }
+        player.openGui(MaxCraft.instance, GUI_ID, world, pos.getX(), pos.getY(), pos.getZ());
+        return true;
+    }
+    
 
 }
---
+++ src/main/java/com/sparks/maxcraft/tilentities/DistilleryTileEntity.java
@@ -0,0 +1,1 @@
+package sparks.maxcraft.tilentities;
