e7120bc8a47707c2267cb2362cde1873fb5ff7e7
SparksCool <70763188+SparksCool@users.noreply.github.com>
2021-04-29 19:43:14 -0400

    nvm

--- src/main/java/com/sparks/maxcraft/blocks/Distillery.java
+++ src/main/java/com/sparks/maxcraft/blocks/Distillery.java
@@ -4,11 +4,23 @@ import sparks.maxcraft.tilentities.*;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumHand;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.entity.player.EntityPlayer;
+
 public class Distillery extends Block {
     public Distillery() {
         super(Material.ROCK);
@@ -20,25 +32,6 @@ public class Distillery extends Block {
     @SideOnly(Side.CLIENT)
     public void initModel() {
         ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
-    }
-    @Override
-    public TileEntity createNewTileEntity(World worldIn, int meta) {
-        return new DistilleryTileEntity();
-    }
-
-    @Override
-    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, ItemStack heldItem, EnumFacing side,
-                float hitX, float hitY, float hitZ) {
-        // Only execute on the server
-        if (world.isRemote) {
-            return true;
-        }
-        TileEntity te = world.getTileEntity(pos);
-        if (!(te instanceof TestContainerTileEntity)) {
-            return false;
-        }
-        player.openGui(MaxCraft.instance, GUI_ID, world, pos.getX(), pos.getY(), pos.getZ());
-        return true;
     }
     
 
--- src/main/java/com/sparks/maxcraft/tilentities/DistilleryTileEntity.java
+++ src/main/java/com/sparks/maxcraft/tilentities/DistilleryTileEntity.java
@@ -1,1 +1,13 @@
 package sparks.maxcraft.tilentities;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+
+public class DistilleryTileEntity extends TileEntity {
+
+ 
+}
